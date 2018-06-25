c2eee0e27c37dd99a8f6b084a5a44e30c2e80ffa
Unknown <steven.chou@uniqestate.com>
2018-06-25 17:24:42 +0800

    JSON CBV

--- rest_api/src/restapi/urls.py
+++ rest_api/src/restapi/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from updates.views import json_example_view
-from updates.views import json_shortcutExample_view
+from updates.views import json_example_view, json_shortcutExample_view, JsonCBV, JsonCBV2
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^json/example/$', json_example_view),
-    url(r'^json/example/shortcut/$', json_shortcutExample_view)
+    url(r'^json/example/shortcut/$', json_shortcutExample_view),
+    url(r'^json/cbv/$', JsonCBV.as_view()),
+    url(r'^json/cbv2/$', JsonCBV2.as_view()),
 ]
--- rest_api/src/updates/views.py
+++ rest_api/src/updates/views.py
@@ -1,6 +1,10 @@
 import json
+
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
+
+from restapi.mixins import JsonResponseMixin
 
 from .models import Update
 # def detail_view(request):
@@ -28,3 +32,24 @@ def json_example_view(request):
     }
     json_data = json.dumps(data)
     return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 3003,
+            "content": "Some new content"
+        }
+
+        json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 3002,
+            "content": "Some new content"
+        }
+
+        return self.render_to_json_response(data)
