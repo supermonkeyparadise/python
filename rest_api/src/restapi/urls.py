c50233082c3cb1ab528b039e0e40faee7e0a5045
Unknown <steven.chou@uniqestate.com>
2018-06-25 17:57:35 +0800

    Serialize Data

--- rest_api/src/restapi/urls.py
+++ rest_api/src/restapi/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from updates.views import json_example_view, json_shortcutExample_view, JsonCBV, JsonCBV2
+from updates.views import (json_example_view, json_shortcutExample_view,
+                           JsonCBV, JsonCBV2, SerializedListView, SerializedDetailView)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +25,6 @@ urlpatterns = [
     url(r'^json/example/shortcut/$', json_shortcutExample_view),
     url(r'^json/cbv/$', JsonCBV.as_view()),
     url(r'^json/cbv2/$', JsonCBV2.as_view()),
+    url(r'^json/serialized/list/$', SerializedListView.as_view()),
+    url(r'^json/serialized/detail/$', SerializedDetailView.as_view()),
 ]
--- rest_api/src/updates/views.py
+++ rest_api/src/updates/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -10,6 +11,8 @@ from .models import Update
 # def detail_view(request):
 #     return render(request, template, {}) # return JSON data
 #     return HttpResponse(get_template().render({}))
+
+# level 1
 
 
 def json_shortcutExample_view(request):
@@ -33,6 +36,8 @@ def json_example_view(request):
     json_data = json.dumps(data)
     return HttpResponse(json_data, content_type='application/json')
 
+# level 2
+
 
 class JsonCBV(View):
     def get(self, request, *args, **kwargs):
@@ -44,6 +49,8 @@ class JsonCBV(View):
         json_data = json.dumps(data)
         return HttpResponse(json_data, content_type='application/json')
 
+# level 3
+
 
 class JsonCBV2(JsonResponseMixin, View):
     def get(self, request, *args, **kwargs):
@@ -53,3 +60,32 @@ class JsonCBV2(JsonResponseMixin, View):
         }
 
         return self.render_to_json_response(data)
+
+
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj], fields=('user', 'content'))
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+
+        # json_data = json.dumps(data)
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        print(data)
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+
+        # json_data = json.dumps(data)
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
