b10eb8fc4c5b1d636328c1750035d51509c4ef1b
Unknown <steven.chou@uniqestate.com>
2018-06-27 16:05:15 +0800

    Boolean expressions, True and False

---
+++ the_complete_python_&_postgreSQL_developer_course/magic_numbers_in_python/boolean_expression.py
@@ -0,0 +1,11 @@
+greater_than_three = 5 > 3
+print(greater_than_three)
+print()
+
+equal_to_five = 5 == 5
+print(equal_to_five)
+print()
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for number in numbers:
+    print(number > 5)
