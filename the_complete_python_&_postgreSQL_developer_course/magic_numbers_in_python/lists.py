78f45b52876febae91210d66e8d5f695541bc1cf
Unknown <steven.chou@uniqestate.com>
2018-06-27 15:32:18 +0800

    Lists in Python

---
+++ the_complete_python_&_postgreSQL_developer_course/magic_numbers_in_python/lists.py
@@ -0,0 +1,5 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('List 長度：{}'.format(len(numbers)))
+print('List 的第二個位置為：{}'.format(numbers[1]))
+print('List 的第五個位置為：{}'.format(numbers[4]))
+print('List 的最後一個值為：{}'.format(numbers[len(numbers)-1]))
