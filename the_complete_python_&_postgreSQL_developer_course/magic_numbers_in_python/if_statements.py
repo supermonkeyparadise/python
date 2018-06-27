c738437f2a02f242486e7212934a2babda65221e
Unknown <steven.chou@uniqestate.com>
2018-06-27 16:15:23 +0800

    If statements in Python

---
+++ the_complete_python_&_postgreSQL_developer_course/magic_numbers_in_python/if_statements.py
@@ -0,0 +1,11 @@
+if 5 > 3:
+    print('Five is greater than three!')
+
+if 3 > 5:
+    print('This should not happen!')
+
+print()
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for number in numbers:
+    if number > 5:
+        print(number)
