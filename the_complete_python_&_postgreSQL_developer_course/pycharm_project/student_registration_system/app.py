cb799ed89caaf76e949b19e084d57799809b317c
Unknown <steven.chou@uniqestate.com>
2018-06-29 17:36:59 +0800

    Student Registration System Code

--- the_complete_python_&_postgreSQL_developer_course/pycharm_project/student_registration_system/app.py
+++ the_complete_python_&_postgreSQL_developer_course/pycharm_project/student_registration_system/app.py
@@ -1,4 +1,5 @@
-student_list=[]
+student_list = []
+
 
 def create_student():
     name = input('Please enter the new student\'s name:')
@@ -31,13 +32,35 @@ def print_student_details(student):
 
 
 def print_student_list(students):
-    for student in students:
+    for i, student in enumerate(students):
+        print('ID: {}'.format(i))
         print_student_details(student)
 
 
-s = create_student()
+def menu():
+    selection = input(
+        'Enter \'p\' to print the student list,'
+        ' \'s\' to add a new student,'
+        ' \'a\' to add a mark to a student,'
+        ' or \'q\' to quit.'
+        ' Enter your selection: ')
 
-add_mark(s, 5)
-add_mark(s, 7)
+    while selection != 'q':
+        if selection == 'p':
+            print_student_list(student_list)
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'a':
+            student_id = int(input('Enter the student ID to add a mark to: '))
+            new_mark = int(input('Enter the new mark to be added: '))
+            add_mark(student_list[student_id], new_mark)
 
-print(calculate_average_mark(s))
+        selection = input(
+            'Enter \'p\' to print the student list,'
+            ' \'s\' to add a new student,'
+            ' \'a\' to add a mark to a student,'
+            ' or \'q\' to quit.'
+            ' Enter your selection: ')
+
+
+menu()
