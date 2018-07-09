3d30a78b0cd401e31708d52684c250ecf2de53b0
Unknown <steven.chou@uniqestate.com>
2018-07-09 18:01:45 +0800

    Obtaining a cursor from the ConnectionFromPool class

--- the_complete_python_&_postgreSQL_developer_course/intro-to-database/project/database.py
+++ the_complete_python_&_postgreSQL_developer_course/intro-to-database/project/database.py
@@ -8,16 +8,23 @@ connection_pool = pool.SimpleConnectionPool(1,
                                             host='localhost')
 
 
-class ConnectionFromPool:
+class CursorFromConnectionFromPool:
     def __init__(self):
         self.connection = None
+        self.cursor = None
 
     # for with clause
     def __enter__(self):
         self.connection = connection_pool.getconn()
-        return self.connection  # connection has attribute 'cursor'
+        self.cursor = self.connection.cursor()
+        return self.cursor
 
     # for with clause
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.commit()
+        if exc_val is not None:
+            self.connection.rollback()
+        else:
+            self.cursor.close()
+            self.connection.commit()
+
         connection_pool.putconn(self.connection)
