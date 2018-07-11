61400ac0bc28bda86e35969f40819ff01df27f45
Unknown <steven.chou@uniqestate.com>
2018-07-11 13:26:43 +0800

    Getting authorization by the user

--- the_complete_python_&_postgreSQL_developer_course/intro-to-oauth/project/login.py
+++ the_complete_python_&_postgreSQL_developer_course/intro-to-oauth/project/login.py
@@ -14,3 +14,17 @@ if response.status != 200:
 
 # convert the query str into a dictionary
 request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+# [Step 2] Getting authorization by the user
+#   Authorization means to give permission
+#   [web workflow]
+#       1. www.ourwebsite.com "log in with twitter button"
+#       2. they press "Sign in" or "authorize"
+#       3. Twitter sends them back e.q. www.ourwebsite.com/auth
+#       4. We get that auth code + request token -> twitter -> access token
+#
+
+print('Go to the following site in your browser:')
+print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+
+oauth_verifier = print('What is the PIN? ')
