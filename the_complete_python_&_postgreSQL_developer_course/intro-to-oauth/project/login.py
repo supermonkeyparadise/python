a108a5ec90d64784bd9da2bea158ceee8d46f2e5
Unknown <steven.chou@uniqestate.com>
2018-07-11 13:57:40 +0800

    Getting the OAuth access token

--- the_complete_python_&_postgreSQL_developer_course/intro-to-oauth/project/login.py
+++ the_complete_python_&_postgreSQL_developer_course/intro-to-oauth/project/login.py
@@ -27,4 +27,15 @@ request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 print('Go to the following site in your browser:')
 print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
 
-oauth_verifier = print('What is the PIN? ')
+oauth_verifier = input('What is the PIN? ')
+
+# [Step 3] Getting the OAuth access token
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+# convert the query str into a dictionary
+request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(request_token)
